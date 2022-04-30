1fa1aaff5bb79a312858dc563c8372b9324393f7
fervillalbag <21fervillalbag@gmail.com>
2022-04-30 04:51:02 -0400

    feat: about api create/update/get

---
+++ models/about.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, models, Model } from 'mongoose'
+import { AboutInfo } from '../src/interfaces/AboutInfo'
+
+const AboutSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: Array,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Number,
+    required: true
+  }
+})
+
+const AboutModel: Model<AboutInfo> =
+  models.About || mongoose.model('About', AboutSchema)
+
+export default AboutModel
