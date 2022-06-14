082cb5e2eb94c83981932bc07c26c70a57c4f114
fervillalbag <21fervillalbag@gmail.com>
2022-06-13 21:46:00 -0400

    remove bg gray at times button

--- packages/web/src/components/Navbar.tsx
+++ packages/web/src/components/Navbar.tsx
@@ -126,6 +126,8 @@ const Navbar: React.FC = () => {
 						_focus={{ boxShadow: '0' }}
 						backgroundColor={`transparent`}
 						onClick={() => setShowNavbar(false)}
+						_hover={{ backgroundColor: `transparent` }}
+						_active={{ backgroundColor: `transparent` }}
 					>
 						<FaTimes />
 					</Button>
